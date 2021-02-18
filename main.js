037a616665e1ed8969813b9925ef0f3ea714f24e
Luisa Vaca <luisavaca@Luisas-iMac.local>
2021-02-17 22:32:34 -0500

    for if and create new instance of vue

---
+++ main.js
@@ -0,0 +1,12 @@
+const myApp = new Vue({
+    el: "#app",
+    data: {
+        title: "Hello mundo",
+        fruits: ["banana", "apple", 'peach'],
+        //objects:
+        animals: [ 
+            {name: "dove", type: "bird", show: true}, 
+            { name: "crow", type: "bird", show: false} 
+        ]
+    }
+})
